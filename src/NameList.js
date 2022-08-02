113bcd8dfaaf68d8639d28652a99e613375cd861
SHUBHANGINI SHARMA <SHUBHANGINISHARMA67@gmail.com>
2022-08-02 19:57:09 +0530

    Commit

--- src/NameList.js
+++ src/NameList.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function NameList() {
     const names=['brew','may','lily']
-    const nameList=names.map(name =><h2>{name}</h2>)
+    const nameList=names.map(name =><h2 key={name}>{name}</h2>)
   return (
     <div>
         {nameList}
